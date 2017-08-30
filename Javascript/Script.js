2c1f7d82900c44e7d9a157863187b5798837b39b
Forrest Berrey <forrest.berrey@coupa.com>
2017-08-29 23:53:32 -0700

    Add initial files

---
+++ Javascript/Script.js
@@ -0,0 +1,26 @@
+//example JS update CSS
+//document.getElementById("demo").style.fontSize = "25px"
+
+//example JS update html
+//document.getElementById("demo").innerHTML = "Hello JavaScript";
+$('#mathz').click(function(){
+multiplier()
+document.getElementById("mathsanswer").innerHTML = answer
+});
+
+function multiplier(){
+    
+    var xNumber1 = document.getElementById("number1")
+    var yNumber2 = document.getElementById("number2")
+    potato(xNumber1, yNumber2)
+    var answer = productvar
+    return answer
+}
+
+function potato(number1, number2){
+    var x = number1
+    var y = number2
+    var productvar = 0
+    productvar = x * y
+    return productvar
+};
